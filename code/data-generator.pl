70a05e9581bbf2613580dccf5da58ef108bce7ed
davidnoahball <davidnoahball@gmail.com>
2017-01-05 11:20:52 -0800

    Set variables to be used in future logic and added comments

--- code/data-generator.pl
+++ code/data-generator.pl
@@ -5,31 +5,48 @@ use diagnostics;
 use feature 'say';
 
 sub data_generator{
-  my ($node_count, $treetop, $complete) = @_;
+  my ($node_limit, $treetop, $complete) = @_; #"node" and "change" are interchangeable terms
 
-  $node_count ||= 32; #number of nodes in tree
+  $node_limit ||= 32; #number of nodes in tree
   $treetop ||= 3; #layers of only choice pages, too make sure the book's stories aren't too short
   $complete ||= 0; #do all paths lead to endings, 0 for false
 
-  my @dummy_data = (
+  my $node_count = 1; #keeps track of the current number of nodes
+  my $available_positions = 2; #keeps track of available positions for nodes to be added
+  my $ending = 0; #toggle: when true, all future nodes will be made endings
+  my $next_page_id = 2; #increments on each new page creation
+
+  my @dummy_data = ( #it can be assumed that this will always be ordered by page id, where (index = id - 1)
     {
-      id => 1, 
-      child1id => 2, 
-      child2id => 3,
-      end => 0
-    },
-    {
-      id => 2, 
-      child1id => 4, 
-      child2id => 0,
-      end => 0
+      id => 1, #page identifier
+      child1id => 0, #where the page leads on one choice
+      child2id => 0, #where the page leads on the other choice
+      end => 0 #is this page an ending (no children)?
     }
   );
+  
+  #a non-directional (doesn't search for openings) tree-descension is inefficient but necessary for accuracy without (much more) complexity
+  while ($node_count < $node_limit){
+    if ($complete && ($node_count + $available_positions == $node_limit)){ #checks for and toggles the end phase
+      $ending = 1;
+    }
+    if ($ending){ #loops through all available positions and sets them to ending pages
+
+    }
+  }
 
   return @dummy_data;
 }
 
-sub print_data{ #doesn't print info in order, but that's not so important
+#start with first page
+#loop{
+#pick random child
+#if child is empty slot (childid=0), add page
+#if child is end (childid=2, id2.end=1), restart
+#if child is choice (childid=2, id2.end=0), restart loop with this page
+#}
+
+sub print_data{ #doesn't print page info in order, but that's not so important for its purpose
   my @data = @_;
 
   say "(";
