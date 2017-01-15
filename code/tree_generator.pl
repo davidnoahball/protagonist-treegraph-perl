95b907feca32469926bae84f6c5bd789f1ba8044
davidnoahball <davidnoahball@gmail.com>
2017-01-14 18:42:22 -0800

    Split depth information into right and left sections in tree_generator

--- code/tree_generator.pl
+++ code/tree_generator.pl
@@ -10,31 +10,48 @@ require "/Users/davidball/Development/code/protagonist-treegraph-perl/code/data_
 sub tree_generator{
   my @tree_data = @_;
 
-  my @unsorted_depths = []; #Whenever a depth is found, either add it to a new array in this one or add it to its proper array, by position
+  my @unsorted_right = [[]]; #Whenever a depth value is found, either add it its proper array or create a new array and populate it
+  my @unsorted_left = [[]];
 
   #First find the depth of each node and add it as an attribute.
   for my $page (@tree_data){ #Add information on parents
     $tree_data[${$page}{id} - 1]{parent_hash_ref} = ((first {${$_}{child1id} == ${$page}{id}} @tree_data) || (first {${$_}{child2id} == ${$page}{id}} @tree_data) || (0));
   }
-  for my $page (@tree_data){ #Fill @unsorted_depths with all of the layer information
-    my $depth = 1;
-    my $parent_check = ${$page}{parent_hash_ref};
-    while (1){
-      if (!$parent_check){
+  for my $page (@tree_data){ #Fill the '@unsorted' arrays with all of the layer information
+    my $depth = 0; #holds the depth value, updated on each parent climb
+    my $parent_check = $page; #the page for which to check and parent information, updated recursively
+    my $memory_holder = 0; #holds a layer of memory for simplicity purposes
+    my $is_right_node = 0; #uses $memory_holder to check if the node is a child of the left or right side of the tree
+    while (1){ #keep looping until broken
+      if (!$parent_check){ #if the node doesn't have a parent, break so the depth can be recorded
         last;
-      } else{
-        $depth += 1;
-        $parent_check = ${$parent_check}{parent_hash_ref};
+      } else{ #otherwise, update the values we're keeping track of
+        $depth += 1; #iterate the depth value
+        $is_right_node = $memory_holder; #set to old memory value, used because the right/left determining factor is checked in the next-to-last iteration
+        $memory_holder = ${$parent_check}{id} == 3 ? 1 : 0; #checks if this node is a child of the node with id=3
+        $parent_check = ${$parent_check}{parent_hash_ref}; #updates the node for which to check parents
       }
     }
-    $tree_data[${$page}{id} - 1]{depth} = $depth;
-    if (!$unsorted_depths[($depth - 1)]){
-      push @unsorted_depths, [${$page}{id}]; 
-    } else{
-      push @unsorted_depths[($depth - 1)], ${$page}{id};
+    $tree_data[${$page}{id} - 1]{depth} = $depth; #adds the depth value as an attribute to nodes for future reference
+    if ($depth == 1){next;} #if we're at the top of the tree, skip the next step because it doesn't belong in right nor left
+    if ($is_right_node){ #if it is a node on the right side of the tree, put it in its proper place in the @unsorted_right
+      if (!$unsorted_right[($depth - 1)]){
+        push @unsorted_right, [${$page}{id}];
+      } else{
+        push @unsorted_right[($depth - 1)], ${$page}{id};
+      }
+    } else{ #otherwise put it in the @unsorted_left
+      if (!$unsorted_left[($depth - 1)]){
+        push @unsorted_left, [${$page}{id}];
+      } else{
+        push @unsorted_left[($depth - 1)], ${$page}{id};
+      }
     }
   }
-  for my $layer (@unsorted_depths){ #Print @unsorted_depths for examination
+  for my $layer (@unsorted_right){ #Print @unsorted_depths for examination
+    say join(", ", @{$layer});
+  }
+  for my $layer (@unsorted_left){ #Print @unsorted_depths for examination
     say join(", ", @{$layer});
   }
 }
