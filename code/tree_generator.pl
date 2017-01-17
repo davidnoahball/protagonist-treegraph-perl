615e207c14637b5de2872197de5c4f9d6f4d550c
davidnoahball <davidnoahball@gmail.com>
2017-01-16 19:02:27 -0800

    Finally figured out array referencing; got parameters passed to row_sorter sub

--- code/tree_generator.pl
+++ code/tree_generator.pl
@@ -12,7 +12,6 @@ sub tree_generator{
 
   my @unsorted_right = [[]]; #Whenever a depth value is found, either add it its proper array or create a new array and populate it
   my @unsorted_left = [[]];
-  my @sorted_right = [];
 
   #First find the depth of each node and add it as an attribute.
   for my $page (@tree_data){ #Add information on parents
@@ -37,34 +36,39 @@ sub tree_generator{
     if ($depth == 1){next;} #if we're at the top of the tree, skip the next step because it doesn't belong in right nor left
     if ($is_right_node){ #if it is a node on the right side of the tree, put it in its proper place in the @unsorted_right
       if (!$unsorted_right[($depth - 1)]){
-        push @unsorted_right, [${$page}{id}];
+        push @unsorted_right, [%{$page}];
       } else{
-        push @unsorted_right[($depth - 1)], ${$page}{id};
+        push @unsorted_right[($depth - 1)], %{$page};
       }
     } else{ #otherwise put it in the @unsorted_left
       if (!$unsorted_left[($depth - 1)]){
-        push @unsorted_left, [${$page}{id}];
+        push @unsorted_left, [%{$page}];
       } else{
-        push @unsorted_left[($depth - 1)], ${$page}{id};
+        push @unsorted_left[($depth - 1)], %{$page};
       }
     }
   }
-  for my $layer (@unsorted_right){ #Print @unsorted_depths for examination
-    say join(", ", @{$layer});
-  }
-  for my $layer (@unsorted_left){ #Print @unsorted_depths for examination
-    say join(", ", @{$layer});
-  }
+  shift @unsorted_right;
+  my @sorted_right = row_sorter(\@unsorted_right, 3);
 }
 
 sub row_sorter{
-  my (@tree, @last_it, $apex) = @_;
-  #Create a tree from the apex
+  my @unsorted = @{$_[0]}; #unsorted comes in as [[{k:v, k:v}, {k:v, k:v}], [{k:v, k:v}, {k:v, k:v}]]; children will always be in the next row
+  my $apex = $_[1];
+  #set a value equal to the line extending from the bottom of the tree determined by the apex
+  my @sorted = [];
+  say $apex;
+  print join(", ", @unsorted), "\n";
+  # if ($sorted[($depth - 1)]){
+  #   push @unsorted_right, [${$page}{id}];
+  # } else{
+  #   push @unsorted_right[($depth - 1)], ${$page}{id};
+  # }
 }
 
 my @dummy = data_generator();
 tree_generator(@dummy);
-print_data(@dummy);
+#print_data(@dummy);
 
 sub folded_notes_please_ignore{
   #I've worked out that it makes the most sense to find the abs_pos of the tree nodes in the row sorting phase and this block is just notes so I don't have to keep referencing my notebook. For all intents and purposes, treat left most as centermost and rightmost as outwardmost. The row_sorter method will be recursive and work in the following manner. First, it will choose a random node at the bottom of the tree and climb, setting each of the initial values in a multidimensional array meant to keep track of the sorted positions, similar to the @unsorted_left, for example. Then, again starting at the bottom, it will climb and call the function again within itself whenever it comes across a node with an undocumented brother, using that as the new apex. The result of that call will be added to the previous call by the following method. The leftmost side of the returned multidimensional array will be treated as a line. This line's position will be determined by the nodes the set is being added to; it will be as far left as possible while making sure that on each row it does not intersect with the existing set's nodes and nor does it go further left than the position of the rightmost node on the layer immediately below it. This is likely not clear to any outside readers (difficult to express without visuals), but it will serve as good reference to me.
